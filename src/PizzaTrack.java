b8a47f366c2b2f57612c3fdad47cd89af48e3bfa
Leicin <pereitha99@hotmail.com>
2026-03-12 19:42:36 -0500

    Commit 3: gestion del menu y las pilas

--- src/PizzaTrack.java
+++ src/PizzaTrack.java
@@ -57,3 +57,69 @@ class PilaManual {
     }
 }
 
+
+public class PizzaTrack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PilaManual pilaUndo = new PilaManual();
+        PilaManual pilaRedo = new PilaManual();
+        
+        int opcion = -1;
+
+        while (opcion != 0) {
+            System.out.println("\n--- PIZZA-TRACK (Gestión de Pedidos) ---");
+            System.out.println("1. Registrar Pedido");
+            System.out.println("2. Deshacer (Undo)");
+            System.out.println("3. Rehacer (Redo)");
+            System.out.println("4. Ver pedido actual (Peek)");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione: ");
+            
+            opcion = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Nombre de la pizza: ");
+                    String nombre = sc.nextLine();
+                    String[] ings = new String[3];
+                    for(int i=0; i<3; i++) {
+                        System.out.print("Ingrediente " + (i+1) + ": ");
+                        ings[i] = sc.nextLine();
+                    }
+                    pilaUndo.push(new Pizza(nombre, ings));
+
+                    // Al escribir nuevo, se limpia la posibilidad de rehacer
+                    while(!pilaRedo.isEmpty()) pilaRedo.pop(); 
+                    break;
+
+                case 2:
+                    if (!pilaUndo.isEmpty()) {
+                        //Elimino de la pila principal y la guardo en la secundaria por medio del push
+                        Pizza p = pilaUndo.pop();
+                        pilaRedo.push(p);
+                        System.out.println("Deshecho: " + p.getNombre());
+                    } else System.out.println("Nada que deshacer.");
+                    break;
+
+                case 3:
+                    //Si la fila es diferente de vacio no tendria logica consultar algo que no existe, por lo tanto se valida que no este vacia para luego hacer el proceso de rehacer
+                    if (!pilaRedo.isEmpty()) {
+                        //Aqui hago la inversa del proceso de deshacer, es decir, elimino de la pila secundaria y lo guardo en la principal por medio del push
+                        Pizza p = pilaRedo.pop();
+                        pilaUndo.push(p);
+                        System.out.println("Rehecho: " + p.getNombre());
+                    } else System.out.println("Nada que rehacer.");
+                    break;
+
+                case 4:
+
+                //Peek: Muestra el pedido actual sin modificar las pilas lo que esta registraddo
+                    Pizza actual = pilaUndo.peek();
+                    System.out.println(actual != null ? "En preparación: " + actual : "No hay pedidos.");
+                    break;
+            }
+        }
+    }
+}
+
